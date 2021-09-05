28403d97ea275db287dd4bfdb6ae136bb2eb3e86
senoralikz <alexjm825@gmail.com>
2021-09-04 22:21:24 -0400

    when clicking add contact button, the field values get added to dom

--- scripts.js
+++ scripts.js
@@ -1,5 +1,28 @@
+let firstName;
+let lastName;
+let phoneNumber;
+let contacts = [];
+
 $("#addContact").on("click", function (event) {
   event.preventDefault();
 
-  console.log("you clicked add contact");
+  firstName = $("#firstName:text").val();
+  lastName = $("#lastName:text").val();
+  phoneNumber = $("#phoneNumber").val();
+
+  if (phoneNumber === "") {
+    console.log("enter number");
+  } else {
+    $("#contact-list").html(`
+    <tr>
+      <td>${firstName}</td>
+      <td>${lastName}</td>
+      <td>${phoneNumber}</td>
+    </tr>
+  `);
+  }
+
+  $("#contact-form")[0].reset();
+
+  console.log(firstName, lastName, phoneNumber);
 });
