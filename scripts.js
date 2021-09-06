7d90e995b12155c8a771756f3ee9adb5b2cb3781
senoralikz <alexjm825@gmail.com>
2021-09-06 12:44:22 -0400

    added sort contact by name functionality and display all contacts

--- scripts.js
+++ scripts.js
@@ -1,31 +1,104 @@
 let firstName;
 let lastName;
 let phoneNumber;
+let person = {};
 let contacts = [];
 
-$("#addContact").on("click", function (event) {
+const addContact = function (event) {
   event.preventDefault();
 
-  firstName = $("#firstName:text").val();
-  lastName = $("#lastName:text").val();
-  phoneNumber = $("#phoneNumber").val();
+  person = {
+    firstName: $("#firstName:text").val(),
+    lastName: $("#lastName:text").val(),
+    phoneNumber: $("#phoneNumber").val(),
+  };
 
-  if (phoneNumber === "") {
+  if (person.phoneNumber === "") {
     $(".message").html(`
-    <div class="alert alert-warning" role="alert">
+    <p class="alert alert-warning" role="alert">
     Please enter a phone number!
-    </div>`);
+    </p>`);
+    $(".alert").delay(2000).fadeOut("slow");
   } else {
-    $("#contact-list").html(`
-    <tr>
-      <td>${firstName}</td>
-      <td>${lastName}</td>
-      <td>${phoneNumber}</td>
-    </tr>
-  `);
-
+    // console.log(person);
+    contacts.push(person);
+    // console.log(contacts);
     $("#contact-form")[0].reset();
   }
 
-  // console.log(firstName, lastName, phoneNumber);
-});
+  let contactsHtml = "";
+
+  contacts.forEach(function (currentContact) {
+    const currentContactHtml = `
+    <tr>
+        <td>${currentContact.firstName}</td>
+        <td>${currentContact.lastName}</td>
+        <td>${currentContact.phoneNumber}</td>
+      </tr>
+    `;
+
+    contactsHtml += currentContactHtml;
+  });
+
+  $("#contact-list").html(contactsHtml);
+};
+
+const sortFirstName = function () {
+  console.log("you clicked on first name table header");
+
+  console.log(contacts);
+
+  contacts.sort((a, b) =>
+    a.firstName > b.firstName ? 1 : b.firstName > a.firstName ? -1 : 0
+  );
+
+  console.log(contacts);
+
+  let contactsHtml = "";
+
+  contacts.forEach(function (currentContact) {
+    const currentContactHtml = `
+      <tr>
+          <td>${currentContact.firstName}</td>
+          <td>${currentContact.lastName}</td>
+          <td>${currentContact.phoneNumber}</td>
+        </tr>
+      `;
+
+    contactsHtml += currentContactHtml;
+  });
+
+  $("#contact-list").html(contactsHtml);
+};
+
+const sortLastName = function () {
+  console.log("you clicked on last name table header");
+
+  console.log(contacts);
+
+  contacts.sort((a, b) =>
+    a.lastName > b.lastName ? 1 : b.lastName > a.lastName ? -1 : 0
+  );
+
+  console.log(contacts);
+
+  let contactsHtml = "";
+
+  contacts.forEach(function (currentContact) {
+    const currentContactHtml = `
+      <tr>
+          <td>${currentContact.firstName}</td>
+          <td>${currentContact.lastName}</td>
+          <td>${currentContact.phoneNumber}</td>
+        </tr>
+      `;
+
+    contactsHtml += currentContactHtml;
+  });
+
+  $("#contact-list").html(contactsHtml);
+};
+
+$("#addContact").on("click", addContact);
+$("#first-name").on("click", sortFirstName);
+$("#last-name").on("click", sortLastName);
